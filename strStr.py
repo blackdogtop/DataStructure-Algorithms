6e055a3a407cbb6e532002a6d4b454c5f2a0e18d
blackdogtop <840474551@qq.com>
2021-01-18 13:56:10 +0800

    update strStrRabinKarp

--- strStr.py
+++ strStr.py
@@ -20,6 +20,7 @@ class Solution:
             time: O((m-n)n)     m - len(haystack) n- len(needle)
             space: O(1)
         """
+
         def isEqual(haystack: str, needle: str) -> bool:
             """time complexity: O(n)"""
             haystackLength = len(haystack)
@@ -54,7 +55,7 @@ class Solution:
                 index = i
                 while j < n and i < m:
                     if haystack[i] != needle[j]:
-                        i = i - equalNum + 1  # 移动指针i至当前位置与相同元素数加一
+                        i = i - equalNum + 1  # 移动指针i至当前位置与相同元素数差值加一
                         break
                     if j == n - 1:
                         return index
@@ -65,11 +66,43 @@ class Solution:
                 i += 1
         return -1
 
+    def strStrRabinKarp(self, haystack: str, needle: str) -> int:
+        """
+        Rabin Karp
+        Complexity:
+            time: O(N) - N is the length of haystack
+            space: O(1)
+        """
+        haystackLen, needleLen = len(haystack), len(needle)
+        if needleLen > haystackLen: return -1
+
+        # 返回Unicode编码的数字
+        unicodeHaystay = lambda i: ord(haystack[i]) - ord('a')
+        unicodeNeedle = lambda i: ord(needle[i]) - ord('a')
+
+        # 旋转哈希
+        a = 26  # const
+        modulo = 2 ** 31  # 取模
+        # init hash
+        hashHaystack, hashNeedle = 0, 0
+        for i in range(needleLen):
+            hashHaystack = (hashHaystack * a + unicodeHaystay(i)) % modulo
+            hashNeedle = (hashNeedle * a + unicodeNeedle(i)) % modulo
+        if hashHaystack == hashNeedle: return 0
+        # update haystack hash
+        for i in range(1, haystackLen - needleLen + 1):
+            # 此处为使用Rabin Karp的关键提升 将hash更新优化到常数时间复杂度
+            hashHaystack = (hashHaystack * a - unicodeHaystay(i - 1) * pow(a, needleLen, modulo) + unicodeHaystay(i + needleLen - 1)) % modulo
+            if hashHaystack == hashNeedle:
+                return i
+        return -1
+
 
 if __name__ == '__main__':
-    haystack = 'mississippi'
-    needle = 'issip'
+    haystack = 'ababcaababcaabc'
+    needle = 'ababcaabc'
     s = Solution()
     # res = s.strStr(haystack, needle)  # 逐一比较子字符串
-    res = s.strStrTwoPtrs(haystack, needle)  # 双指针
+    # res = s.strStrTwoPtrs(haystack, needle)  # 双指针
+    res = s.strStrRabinKarp(haystack, needle)  # Rabin Karp
     print(res)
